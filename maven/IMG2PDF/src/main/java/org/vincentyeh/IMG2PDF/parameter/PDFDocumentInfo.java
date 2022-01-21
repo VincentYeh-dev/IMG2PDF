f67cc718ff4e17b05aee8d7c8aecd35253d5b0b0
VincentYeh-dev <vincent3314534@gmail.com>
2022-01-21 18:27:57 +0800

    Change PDFDocumentInfo from interface to class.

--- maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/parameter/PDFDocumentInfo.java
+++ maven/IMG2PDF/src/main/java/org/vincentyeh/IMG2PDF/parameter/PDFDocumentInfo.java
@@ -1,9 +1,49 @@
 package org.vincentyeh.IMG2PDF.parameter;
 
-public interface PDFDocumentInfo {
-    String getTitle();
-    String getAuthor();
-    String getCreator();
-    String getProducer();
-    String getSubject();
+public class PDFDocumentInfo {
+    private String title;
+    private String author;
+    private String creator;
+    private String producer;
+    private String subject;
+
+    public final String getTitle() {
+        return title;
+    }
+
+    public final String getAuthor() {
+        return author;
+    }
+
+    public final String getCreator() {
+        return creator;
+    }
+
+    public final String getProducer() {
+        return producer;
+    }
+
+    public final String getSubject() {
+        return subject;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
